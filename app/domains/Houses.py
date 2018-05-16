887f3cf080684e1ae0c19f76b64e2fdf07dfbc17
宋城 <songcheng@yintong.com.cn>
2018-05-16 13:45:09 +0800

    销控表

--- app/domains/Houses.py
+++ app/domains/Houses.py
@@ -16,22 +16,9 @@ def get_building_num_by_house_name(id):
     return {'data':list(build_nums)}
 
 def get_building_by_name(id,build):
-    # rooms = db.session.query(HouseDetail).filter(HouseDetail.house==id).filter(HouseDetail.building_num==build).all()
-    # ret = []
-    # rooms.sort(key=lambda x:int(re.search(r'(\d+)室',x.room_num).group(1)))
-    # room_nums = sorted(list(set([re.search(r'(0\d)室',room.room_num).group(1) for room in rooms])))
-    # n = 0
-    # last = len(room_nums) - 1
-    # for index in range(len(rooms))[:-last:len(room_nums)]:
-    #     m = 0
-    #     for count in room_nums:
-    #         if len(ret) < n+1:
-    #             ret.append({})
-    #         ret[n][count]='{}/{}/{}/{}'.format(rooms[index+m].room_num,rooms[index+m].floor_area,rooms[index+m].unit_price,rooms[index+m].total_price)
-    #         m += 1
-    #     n+=1
-    # return ret
+
     ret = []
+    #过滤那些特殊楼幢
     builds = db.session.query(HouseDetail.building_num,db.func.count(HouseDetail.building_num)).filter(HouseDetail.house==id).filter(HouseDetail.building_num.startswith(build)).group_by(HouseDetail.building_num).all()
     max = 0
     for i in builds:
@@ -39,17 +26,15 @@ def get_building_by_name(id,build):
             max = i[1]
     unit_nums = [build[0]for build in builds if build[1] == max]
 
+    #获取楼幢号和房号
     rooms = HouseDetail.query.filter(HouseDetail.house == id).filter(HouseDetail.building_num.in_(unit_nums)).all()
     rooms.sort(key=lambda x: int(re.search(r'(\d+)室', x.room_num).group(1)))
     room_nums = sorted(list(set([re.search(r'(\d\d)室', room.room_num).group(1) for room in rooms])))
 
-    last = len(room_nums) + len(unit_nums) - 1
     cloumn_name = [unit + room for unit in unit_nums for room in room_nums]
     floors = sorted(list(set([int(re.search(r'^(\d+)\d\d室', room.room_num).group(1)) for room in rooms])))
-    print(floors)
+    #根据楼层循环
     for floor in floors:
-    # for index in range(len(rooms))[:-last:len(room_nums)+len(unit_nums)]:
-    #     one_floor = rooms[index:index+len(room_nums)+len(unit_nums)]
         data = {}
         for m in unit_nums:
             for n in room_nums:
